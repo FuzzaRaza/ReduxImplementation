12fce6834d8850a1f8038d63cbe6f59b98badc1f
MAJU STUDENT <majustudent@jinnah.edu>
2019-05-05 18:02:45 +0500

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,31 @@
+//Header Control
+//Imports
+import React from "react";
+import { Text, View } from "react-native";
+//Creation
+const Header = props => {
+const { textStyle, viewStyle } = styles;
+return (
+<View style={viewStyle}>
+    <Text style={textStyle}> {props.headerText}</Text>
+</View>
+);
+};
+//Defining the Styles
+const styles = {
+viewStyle: {
+backgroundColor: "#F8F8F8",
+justifyContents: "center",
+alignItems: "center",
+height: 50,
+paddingTop: 15,
+position: "relative"
+},
+textStyle: {
+fontSize: 20,
+backgroundColor: "white"
+}
+};
+//Availability to other parts
+// export default Header;
+export { Header };
